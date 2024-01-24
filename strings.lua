8fb04e4ab3bd1659f1ce7157509c0b340a290b22
Florian Thiel <flth@flth.de>
2024-01-24 23:56:45 +0100

    Texte überarbeitet

--- strings.lua
+++ strings.lua
@@ -3,36 +3,34 @@ return {
 	en = {
 
 		["mod_name"] = "DB Class 670 double deck rail car",
-		["mod_desc"] = "The class 670 is a double-decker diesel railcar. It has two axles and contains parts from bus construction. They were built from 1996 onwards, a successor series was never built. The trains were used on some lines, especially in Thuringia and on the Moselle line. As there were frequent problems, especially with the engine cooling, they were removed from active service. Half of the vehicles have been parked since 2014, the others are partly still in service.\n\nAvilibility: from 1996\nMaximum speed: 100 km/h\nCapacity: 28",
+		["mod_desc"] = "The class 670 is a double-decker diesel railcar. It has two axles and contains parts from bus construction. They were built from 1996 onwards, a successor series was never built. The trains were used on some lines, especially in Thuringia and on the Moselle line. As there were frequent problems, especially with the engine cooling, they were removed from active service. Half of the vehicles have been parked since 2014, the others are partly still in service.\n\nAvilibility: 1996-2014\nMaximum speed: 100 km/h\nCapacity: 28",
 
         ["br670_menu"] = "Class 670 double deck rail car",
 
-		["br670_proto_name"] = "Class 670 (Demonstrator)",
-        ["br670_1_name"] = "Class 670 002 first livery",
-		["br670_2_name"] = "Class 670 006 (\"Alfred\")",
-		["br670_3_name"] = "Class 670 005 (\"Elfriede\")",
-		["br670_4_name"] = "Class 670 002 (\"Alma\")",
-		["br670_5_name"] = "VT 670.3 Prignitzer Eisenbahn",
-		["br670_6_name"] = "Class 670 002 DB Regio",
-        ["br670_desc"] = "The double-decker rail buses had been in service since 1996, mainly on routes in Thuringia and on the Moselle. Due to persistent problems with the engine cooling and air conditioning, they were largely parked until 2014, some can still be found in museum use today.",
+        ["br670_1_name"] = "Class 670 002 first livery (1996)",
+		["br670_2_name"] = "Class 670 006 (\"Alfred\") (1996)",
+		["br670_3_name"] = "Class 670 005 (\"Elfriede\") (1996)",
+		["br670_4_name"] = "Class 670 002 (\"Alma\") (1996)",
+		["br670_5_name"] = "VT 670.3 Prignitzer Eisenbahn (1996)",
+		["br670_6_name"] = "Class 670 002 DB Regio (1996)",
+        ["br670_desc"] = "The double-decker rail buses had been in service since 1996, mainly on routes in Thuringia and at the Moselle. Due to persistent problems with the engine cooling and air conditioning, they were largely parked until 2014, some can still be found in museum use today.",
 
 	},
 	de = {
 
 		["mod_name"] = "BR 670 Doppelstock-Schienenbus",
-		["mod_desc"] = "Die Baureihe 670 ist ein doppelstöckiger Dieseltriebwagen. Er ist zweiachsig und enthält Teile aus dem Omnibusbau. Gebaut wurden sie ab 1996, eine Nachfolgerserie wurde nie gebaut. Die Zügen waren auf einigen Strecken unterwegs vor allem in Thürigen und auf der Moselstrecke. Da es häufige Probleme vor allem mit der Motorkühlung gab, wurden sie aus dem aktiven Dienst entfernt. Die Hälfte der Fahrzeuge ist seit 2014 abgestellt, die anderen sind teilweise noch im Einsatz.\n\nVerfügbarkeit: ab 1996\nHöchstgeschwindigkeit: 100 km/h\nKapazität: 28",
+		["mod_desc"] = "Die Baureihe 670 ist ein doppelstöckiger Dieseltriebwagen. Er ist zweiachsig und enthält Teile aus dem Omnibusbau. Gebaut wurden sie ab 1996, eine Nachfolgerserie wurde nie gebaut. Die Zügen waren auf einigen Strecken unterwegs vor allem in Thürigen und auf der Moselstrecke. Da es häufige Probleme vor allem mit der Motorkühlung gab, wurden sie aus dem aktiven Dienst entfernt. Die Hälfte der Fahrzeuge ist seit 2014 abgestellt, die anderen sind teilweise noch im Einsatz.\n\nVerfügbarkeit: 1996-2014\nHöchstgeschwindigkeit: 100 km/h\nKapazität: 28",
 
         ["br670_menu"] = "BR 670 Doppelstock-Schienenbus",
 
-		["br670_proto_name"] = "BR 670 (Demonstrator)",
-        ["br670_1_name"] = "BR 670 002 Erstlackierung",
-		["br670_2_name"] = "BR 670 006 (\"Alfred\")",
-		["br670_3_name"] = "BR 670 005 (\"Elfriede\")",
-		["br670_4_name"] = "BR 670 002 (\"Alma\")",
-		["br670_5_name"] = "VT 670.3 Prignitzer Eisenbahn",
-		["br670_6_name"] = "BR 670 002 DB Regio",
-        ["br670_desc"] = "Die Doppelstock-Schienenbusse waren seit 1996 im Einsatz vor allem auf Strecken in Thüringen und an an der Mosel. Wegen anhaltender Probleme mit der Motorkühlung und Klimaanlage wurden sie bis 2014 weitgehend abgestellt, einige sind heute noch im Museumseinsatz zu finden.",
- 
+        ["br670_1_name"] = "BR 670 002 Erstlackierung (1996)",
+		["br670_2_name"] = "BR 670 006 (\"Alfred\") (1996)",
+		["br670_3_name"] = "BR 670 005 (\"Elfriede\") (1996)",
+		["br670_4_name"] = "BR 670 002 (\"Alma\") (1996)",
+		["br670_5_name"] = "VT 670.3 Prignitzer Eisenbahn (1996)",
+		["br670_6_name"] = "BR 670 002 DB Regio (1996)",
+        ["br670_desc"] = "Die Doppelstock-Schienenbusse waren seit 1996 im Einsatz vor allem auf Strecken in Thüringen und an der Mosel. Wegen anhaltender Probleme mit der Motorkühlung und Klimaanlage wurden sie bis 2014 weitgehend abgestellt, einige sind heute noch im Museumseinsatz zu finden.",
+
 	}
 }
 end
