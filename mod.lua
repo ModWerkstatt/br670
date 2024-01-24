d804e3f9bd85754786f31abbcd8465da87d1d042
Florian Thiel <flth@flth.de>
2024-01-24 12:16:02 +0100

    Releaseversion

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "train", "diesel", "europe", "double deck", "doppeldecker", "multiple unit" },
+		dependencies = { },
+		url = { },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+}
+end
